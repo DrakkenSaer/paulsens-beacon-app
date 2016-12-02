74b72d8b3e8df2b1e01b12e224fc9078ef6f5f77
drakkensaer <shadowdrakkeen@gmail.com>
2016-12-02 21:20:58 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
+  rolify
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
   store_accessor :preferences
+  
+  after_create :assign_default_role
+
+  def assign_default_role
+    self.add_role(:standard) if self.roles.blank?
+  end
 end
