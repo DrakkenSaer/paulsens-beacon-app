1ecab4c45ca520984d656866da4055f714f0aee8
Ewa <kocica@gmail.com>
2017-01-30 19:17:44 +0000

    Add working invalid parameter tests for update

--- spec/controllers/beacons_controller_spec.rb
+++ spec/controllers/beacons_controller_spec.rb
@@ -53,13 +53,13 @@ RSpec.describe BeaconsController, type: :controller do
   
   describe "POST #create" do
     let (:valid_params) { { beacon: { title: 'test', description: 'test' } } }
-    let (:invalid_params) { { beacon: { cake: 'test' } } }
+    let (:invalid_params) { { beacon: { title: nil } } }
 
     context "as user" do
       login_user
       it "should raise an exception if not an admin" do
         expect do
-          post :create, valid_params
+          post :create, params: valid_params
         end.to raise_error(Pundit::NotAuthorizedError)
       end
     end
@@ -70,12 +70,12 @@ RSpec.describe BeaconsController, type: :controller do
       context "with valid parameters" do
         it "increases amount of beacons by 1" do
           expect {
-            post :create, valid_params
+            post :create, params: valid_params
           }.to change(Beacon, :count).by(1)
         end
         
         it "redirects to the new beacon" do
-          post :create, valid_params
+          post :create, params: valid_params
           expect(response).to redirect_to Beacon.last
         end
       end
@@ -83,28 +83,28 @@ RSpec.describe BeaconsController, type: :controller do
       context "with invalid parameters" do
         it "does not save new beacon" do
           expect{
-            post :create, invalid_params
+            post :create, params: invalid_params
           }.to_not change(Beacon, :count)
         end
         
         it "re-renders the new method" do
-          post :create, invalid_params
+          post :create, params: invalid_params
           expect(response).to render_template :new
         end
       end
     end
   end
   
-  describe "POST #update" do
+  describe "PUT #update" do
     let (:valid_params) { { title: 'new title', description: 'new description' } }
-    let (:invalid_params) { { cake: 'test' } }
+    let (:invalid_params) { { title: nil } }
 
     context "as user" do
       login_user
       it "should raise an exception if not an admin" do
         test_beacon = FactoryGirl.create(:beacon)
         expect do
-          put :update, { id: test_beacon.id }
+          put :update, params: { id: test_beacon.id }
         end.to raise_error(Pundit::NotAuthorizedError)
       end
     end
@@ -114,7 +114,7 @@ RSpec.describe BeaconsController, type: :controller do
       context "valid_params" do
         before(:each) do
           @test_beacon = FactoryGirl.create(:beacon)
-          put :update, id: @test_beacon.id, beacon: valid_params
+          put :update, params: {id: @test_beacon.id, beacon: valid_params}
           @test_beacon.reload
         end
 
@@ -127,7 +127,40 @@ RSpec.describe BeaconsController, type: :controller do
           expect(response).to redirect_to @test_beacon
         end
       end
+      
+      context "invalid params" do
+        before(:each) do
+          @test_beacon = FactoryGirl.create(:beacon)
+          put :update, params: { id: @test_beacon.id, beacon: invalid_params }
+          @test_beacon.reload
+        end
+      
+        it "should not update object paramaters when given invalid parameters" do
+          expect(@test_beacon.title).to eql "test"
+          expect(@test_beacon.description).to eql "test description"
+        end
+        
+        it "should rerender edit page when update unsuccessful" do
+          expect(response).to render_template :edit
+        end
+      end
     end
   end
+  
+  # describe "DELETE #destroy" do
+  #   before(:each) do
+  #     @test_beacon = FactoryGirl.create(:beacon)
+  #   end
+    
+  #   context "as user" do
+  #     login_user
+      
+  #     it "should raise an exception if not an admin" do
+  #       expect do
+  #       put :update, params: { id: @test_beacon.id }
+  #       end.to raise_error(Pundit::NotAuthorizedError)
+  #     end
+  #   end
+  # end
 
 end
