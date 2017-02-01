55516ba4abcf3f6dec3672678acd12637db6852e
Adam Gorbahn <adamgorbahn@hotmail.com>
2017-02-01 20:37:14 +0000

    add product model for testing

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe Product do
+  it "has a valid factory" do
+    FactoryGirl.create(:product).should be_valid
+  end
+
+  it "is invalid without a featured, output flase" do
+    product = FactoryGirl.create(:product, featured: nil).featured.should == false
+  end
+
+  it "is invalid without a cost" do
+    product = FactoryGirl.create(:product, cost: nil).cost.should == "0.0"
+  end
+  
+  it "is invalid without a description" do
+    product = FactoryGirl.build(:product, description: nil).should_not be_valid
+  end
+  
+  it "is invalid without a title" do
+    product = FactoryGirl.build(:product, title: nil).should_not be_valid
+  end
+
+  it "returns a product featured as a boolean" do
+    FactoryGirl.create(:product, featured: true).featured.should == true
+  end
+  
+  it "returns a product description as a string" do
+    FactoryGirl.create(:product, description: "Bob").description.should == "Bob"
+  end
+  
+  it "returns a product title as a string" do
+    FactoryGirl.create(:product, title: "Bob").title.should == "Bob"
+  end
+  
+  it "returns a product cost as a string" do
+    FactoryGirl.create(:product, cost: "10.99").cost.should == "10.99"
+  end
+
+end
