57a50bcc444b660ab6d611c63c20ecd1ca5960f2
drakkensaer <shadowdrakkeen@gmail.com>
2017-02-03 19:21:31 +0000

    made fixes, merging with master

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,6 +1,7 @@
 class PromotionsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_promotion, except: [:index, :create]
+  before_action :authorize_promotion, except: [:index, :create]
 
   def index
     @promotions = policy_scope(Promotion)
@@ -14,6 +15,9 @@ class PromotionsController < ApplicationController
 
   def create
     @promotion = Promotion.new(promotion_params)
+
+    authorize @promotion
+    
     if @promotion.save
       redirect_to @promotion, success: "Promotion successfully created!"
     else
@@ -37,12 +41,25 @@ class PromotionsController < ApplicationController
     redirect_to promotions_url
   end
   
-private
-  def promotion_params
-    params.require(:promotion).permit(:promotional_id, :promotional_type, :title, :description, :code, :redeem_count, :daily_deal, :featured, :cost, :expiration)
-  end
-  
-  def set_promotion
-    @promotion = Promotion.find(params[:id])
-  end
+  private
+    def promotion_params
+      params.require(:promotion).permit(:promotional_id, 
+                                        :promotional_type, 
+                                        :title, 
+                                        :description, 
+                                        :code, 
+                                        :redeem_count, 
+                                        :daily_deal, 
+                                        :featured, 
+                                        :cost, 
+                                        :expiration)
+    end
+    
+    def set_promotion
+      @promotion = Promotion.find(params[:id])
+    end
+    
+    def authorize_promotion
+      authorize @promotion
+    end
 end
--- spec/controllers/promotions_controller_spec.rb
+++ spec/controllers/promotions_controller_spec.rb
@@ -23,13 +23,6 @@ RSpec.describe PromotionsController, type: :controller do
 #     end
 #   end
 
-#   describe "GET #create" do
-#     it "returns http success" do
-#       get :create
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
 #   describe "GET #edit" do
 #     it "returns http success" do
 #       get :edit
@@ -37,48 +30,47 @@ RSpec.describe PromotionsController, type: :controller do
 #     end
 #   end
 
-#   describe "GET #update" do
-#     it "returns http success" do
-#       get :update
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-
-#   describe "GET #destroy" do
-#     it "returns http success" do
-#       get :destroy
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-  
   describe "POST #create" do
     let (:valid_params) { { promotion: FactoryGirl.attributes_for(:promotion) } }
     let (:invalid_params) { { promotion: FactoryGirl.attributes_for(:promotion, title: nil) } }
-    login_user 
     
-    context "with valid parameters" do
-      it "increases amount of promotions by 1" do
-        expect {
-          post :create, params: valid_params
-        }.to change(Promotion, :count).by(1)
-      end
+    context "as standard user" do
+      login_user
       
-      it "redirects to new promotion" do
-        post :create, params: valid_params
-        expect(response).to redirect_to Promotion.last
+      it "should raise an exception if not an admin" do
+        expect do
+          post :create, params: valid_params
+        end.to raise_error(Pundit::NotAuthorizedError)
       end
     end
     
-    context "with invalid parameters" do
-      it "does not save new promotion" do
-        expect{
-          post :create, params: invalid_params
-        }.to_not change(Promotion, :count)
+    context "as admin user" do
+      login_admin
+      
+      context "with valid parameters" do
+        it "increases amount of promotions by 1" do
+          expect {
+            post :create, params: valid_params
+          }.to change(Promotion, :count).by(1)
+        end
+        
+        it "redirects to new promotion" do
+          post :create, params: valid_params
+          expect(response).to redirect_to Promotion.last
+        end
       end
       
-      it "re-renders the new method" do
-        post :create, params: invalid_params
-        expect(response).to render_template :new
+      context "with invalid parameters" do
+        it "does not save new promotion" do
+          expect{
+            post :create, params: invalid_params
+          }.to_not change(Promotion, :count)
+        end
+        
+        it "re-renders the new method" do
+          post :create, params: invalid_params
+          expect(response).to render_template :new
+        end
       end
     end
   end
@@ -86,52 +78,67 @@ RSpec.describe PromotionsController, type: :controller do
   describe "PUT #update" do
     let (:valid_params) { FactoryGirl.attributes_for(:promotion, title: "new title", description: "new description") }
     let (:invalid_params) { FactoryGirl.attributes_for(:promotion, title: nil) }
-    login_user
-    
-    context "invalid id" do
-      it "should return an ActiveRecord error if the promotion id does not exist" do
+
+    context "as standard user" do
+      login_user
+      
+      it "should raise an exception if not an admin" do
+        test_promotion = FactoryGirl.create(:promotion)
+
         expect do
-           put :update, params: {id: 999, promotion: valid_params}
-        end.to raise_error(ActiveRecord::RecordNotFound)
+          put :update, params: { id: test_promotion, promotion: valid_params }
+        end.to raise_error(Pundit::NotAuthorizedError)
       end
     end
-    
-    context "valid parameters" do
-      before(:each) do
-        @test_promotion = FactoryGirl.create(:promotion)
-        put :update, params: {id: @test_promotion, promotion: valid_params}
-        @test_promotion.reload
+
+    context "as admin user" do
+      login_admin
+
+      context "invalid id" do
+        it "should return an ActiveRecord error if the promotion id does not exist" do
+          expect do
+             put :update, params: {id: -1, promotion: valid_params}
+          end.to raise_error(ActiveRecord::RecordNotFound)
+        end
       end
       
-      it "should find the correct promotion" do
-        expect(assigns(:promotion)).to eql @test_promotion
+      context "valid parameters" do
+        before(:each) do
+          @test_promotion = FactoryGirl.create(:promotion)
+          put :update, params: {id: @test_promotion, promotion: valid_params}
+          @test_promotion.reload
+        end
+        
+        it "should find the correct promotion" do
+          expect(assigns(:promotion)).to eql @test_promotion
+        end
+        
+        it "should update object paramaters" do
+          expect(@test_promotion.title).to eql valid_params[:title]
+          expect(@test_promotion.description).to eql valid_params[:description]
+          expect(@test_promotion.created_at).to_not eql @test_promotion.updated_at
+        end
+        
+        it "should redirect to promotion page when successful" do
+          expect(response).to redirect_to @test_promotion
+        end
       end
       
-      it "should update object paramaters" do
-        expect(@test_promotion.title).to eql valid_params[:title]
-        expect(@test_promotion.description).to eql valid_params[:description]
-        expect(@test_promotion.created_at).to_not eql @test_promotion.updated_at
-      end
-      
-      it "should redirect to promotion page when successful" do
-        expect(response).to redirect_to @test_promotion
-      end
-    end
-    
-    context "with invalid parameters" do
-      before(:each) do
-        @test_promotion = FactoryGirl.create(:promotion)
-        put :update, params: {id: @test_promotion, promotion: invalid_params}
-        @test_promotion.reload
-      end
-      
-      it "should not update object parameters" do
-        expect(@test_promotion.title).to eql FactoryGirl.attributes_for(:promotion)[:title]
-        expect(@test_promotion.created_at).to eql @test_promotion.updated_at
-      end
-      
-      it "should rerender edit page when update unsuccessful" do
-        expect(response).to render_template :edit
+      context "with invalid parameters" do
+        before(:each) do
+          @test_promotion = FactoryGirl.create(:promotion)
+          put :update, params: {id: @test_promotion, promotion: invalid_params}
+          @test_promotion.reload
+        end
+        
+        it "should not update object parameters" do
+          expect(@test_promotion.title).to eql FactoryGirl.attributes_for(:promotion)[:title]
+          expect(@test_promotion.created_at).to eql @test_promotion.updated_at
+        end
+        
+        it "should rerender edit page when update unsuccessful" do
+          expect(response).to render_template :edit
+        end
       end
     end
   end
@@ -140,28 +147,41 @@ RSpec.describe PromotionsController, type: :controller do
     before(:each) do
       @test_promotion = FactoryGirl.create(:promotion)
     end
-    login_user
-    
-    it "should return an ActiveRecord error if the promotion id does not exist" do
-      expect do
-         delete :destroy, params: {id: 999}
-      end.to raise_error(ActiveRecord::RecordNotFound)
+
+    context "as standard user" do
+      login_user
+      
+      it "should raise an exception if not an admin" do
+        expect do
+          delete :destroy, params: { id: @test_promotion }
+        end.to raise_error(Pundit::NotAuthorizedError)
+      end
     end
+
+    context "as admin user" do
+      login_admin
     
-    it "should find the correct promotion" do
-      delete :destroy, params: {id: @test_promotion}
-      expect(assigns(:promotion)).to eql @test_promotion
-    end
-    
-    it "deletes the promotion from the database" do
-      expect do
-         delete :destroy, params: { id: @test_promotion }
-      end.to change(Promotion, :count).by(-1)
-    end
-    
-    it "redirects to the promotion index" do
-      delete :destroy, params: { id: @test_promotion }
-      expect(response).to redirect_to promotions_url
+      it "should return an ActiveRecord error if the promotion id does not exist" do
+        expect do
+           delete :destroy, params: {id: 999}
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
+      
+      it "should find the correct promotion" do
+        delete :destroy, params: {id: @test_promotion}
+        expect(assigns(:promotion)).to eql @test_promotion
+      end
+      
+      it "deletes the promotion from the database" do
+        expect do
+           delete :destroy, params: { id: @test_promotion }
+        end.to change(Promotion, :count).by(-1)
+      end
+      
+      it "redirects to the promotion index" do
+        delete :destroy, params: { id: @test_promotion }
+        expect(response).to redirect_to promotions_url
+      end
     end
   end
     
