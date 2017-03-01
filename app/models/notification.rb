72b59dcfa8d6d76307214572035dda0580b163eb
drakkensaer <shadowdrakkeen@gmail.com>
2017-03-01 21:27:58 +0000

    removed presence validations for exit and enter messages on Notification; It has become apparent that not all notifications will contain both

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :beacon, optional: true
   
-  validates :title, :description, :entry_message, :exit_message, presence: true
+  validates :title, :description, presence: true
   validates :title, :description, uniqueness: true
   
   resourcify
