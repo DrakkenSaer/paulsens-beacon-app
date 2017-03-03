68459da49dfba32138dbf0f5b4fea6041eee333b
drakkensaer <shadowdrakkeen@gmail.com>
2017-03-03 21:42:58 +0000

    update

--- db/migrate/20170303192204_change_points_column_to_int.rb
+++
@@ -1,5 +0,0 @@
-class ChangePointsColumnToInt < ActiveRecord::Migration[5.0]
-  def change
-    change_column :currencies, :value, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170303192204) do
-=======
 ActiveRecord::Schema.define(version: 20170301215131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
->>>>>>> staging
 
   create_table "beacons", force: :cascade do |t|
     t.string   "uuid",        null: false
@@ -27,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170301215131) do
     t.datetime "updated_at",  null: false
     t.string   "major_uuid",  null: false
     t.string   "minor_uuid",  null: false
-    t.index ["uuid"], name: "index_beacons_on_uuid"
+    t.index ["uuid"], name: "index_beacons_on_uuid", using: :btree
   end
 
   create_table "currencies", force: :cascade do |t|
@@ -37,7 +33,7 @@ ActiveRecord::Schema.define(version: 20170301215131) do
     t.integer  "value"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["cashable_type", "cashable_id"], name: "index_currencies_on_cashable_type_and_cashable_id"
+    t.index ["cashable_type", "cashable_id"], name: "index_currencies_on_cashable_type_and_cashable_id", using: :btree
   end
 
   create_table "historical_events", force: :cascade do |t|
@@ -59,8 +55,8 @@ ActiveRecord::Schema.define(version: 20170301215131) do
     t.string   "item_cost",     null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["lineable_type", "lineable_id"], name: "index_line_items_on_lineable_type_and_lineable_id"
-    t.index ["order_id"], name: "index_line_items_on_order_id"
+    t.index ["lineable_type", "lineable_id"], name: "index_line_items_on_lineable_type_and_lineable_id", using: :btree
+    t.index ["order_id"], name: "index_line_items_on_order_id", using: :btree
   end
 
   create_table "notifications", force: :cascade do |t|
@@ -71,22 +67,18 @@ ActiveRecord::Schema.define(version: 20170301215131) do
     t.datetime "updated_at",         null: false
     t.string   "entry_message"
     t.string   "exit_message"
-<<<<<<< HEAD
-    t.index ["beacon_id"], name: "index_notifications_on_beacon_id"
-=======
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.index ["beacon_id"], name: "index_notifications_on_beacon_id", using: :btree
->>>>>>> staging
   end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "user_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -105,35 +97,21 @@ ActiveRecord::Schema.define(version: 20170301215131) do
   create_table "promotions", force: :cascade do |t|
     t.string   "promotional_type"
     t.integer  "promotional_id"
-<<<<<<< HEAD
-    t.string   "title",                                            null: false
-    t.text     "description",                                      null: false
-    t.string   "code",                                             null: false
-    t.integer  "redeem_count",     default: 0,                     null: false
-    t.boolean  "daily_deal",       default: false,                 null: false
-    t.boolean  "featured",         default: false,                 null: false
-    t.integer  "cost",             default: 0,                     null: false
-    t.datetime "expiration",       default: '2017-03-14 20:12:53'
-    t.datetime "created_at",                                       null: false
-    t.datetime "updated_at",                                       null: false
-    t.index ["promotional_type", "promotional_id"], name: "index_promotions_on_promotional_type_and_promotional_id"
-=======
-    t.string   "title",                                              null: false
-    t.text     "description",                                        null: false
-    t.string   "code",                                               null: false
-    t.integer  "redeem_count",       default: 0,                     null: false
-    t.boolean  "daily_deal",         default: false,                 null: false
-    t.boolean  "featured",           default: false,                 null: false
-    t.integer  "cost",               default: 0,                     null: false
-    t.datetime "expiration",         default: '2017-03-15 21:20:30'
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
+    t.string   "title",                              null: false
+    t.text     "description",                        null: false
+    t.string   "code",                               null: false
+    t.integer  "redeem_count",       default: 0,     null: false
+    t.boolean  "daily_deal",         default: false, null: false
+    t.boolean  "featured",           default: false, null: false
+    t.integer  "cost",               default: 0,     null: false
+    t.datetime "expiration"
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.index ["promotional_type", "promotional_id"], name: "index_promotions_on_promotional_type_and_promotional_id", using: :btree
->>>>>>> staging
   end
 
   create_table "roles", force: :cascade do |t|
@@ -142,8 +120,8 @@ ActiveRecord::Schema.define(version: 20170301215131) do
     t.integer  "resource_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-    t.index ["name"], name: "index_roles_on_name"
+    t.index ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+    t.index ["name"], name: "index_roles_on_name", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -164,15 +142,18 @@ ActiveRecord::Schema.define(version: 20170301215131) do
     t.string   "address"
     t.integer  "visit_count",            default: 0
     t.text     "preferences"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["preferences"], name: "index_users_on_preferences"
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["preferences"], name: "index_users_on_preferences", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "role_id"
-    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+    t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
   end
 
+  add_foreign_key "line_items", "orders"
+  add_foreign_key "notifications", "beacons"
+  add_foreign_key "orders", "users"
 end
