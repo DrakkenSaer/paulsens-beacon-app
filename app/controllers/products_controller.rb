46ac8e7d54029501a93ee4c279c5f51f22ce8392
Ewa <kocica@gmail.com>
2017-03-03 00:12:28 +0000

    Make corrections and add handling for lack of associations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -48,7 +48,7 @@ class ProductsController < ApplicationController
     @product.destroy
     
     respond_to do |format|
-      format.html { redirect_to product_url, notice "Product successfully destroyed" }
+      format.html { redirect_to products_url, notice: "Product successfully destroyed" }
       format.json { head :no_content }
     end
   end
