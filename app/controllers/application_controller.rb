a5442b5f9eb30c5ceebc8ce8e7dce68c1a9045df
drakkensaer <shadowdrakkeen@gmail.com>
2017-04-12 21:10:54 +0000

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,38 +2,4 @@ class ApplicationController < ActionController::Base
   include Pundit
   after_action :verify_authorized, except: :index, unless: :devise_controller?
   after_action :verify_policy_scoped, only: :index, unless: :devise_controller?
-
-  protected
-
-    def authenticate_request!
-      unless token_user?
-        render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-        return
-      end
-      @current_user = token_user
-    rescue JWT::VerificationError, JWT::DecodeError
-      render json: { errors: ['Not Authenticated'] }, status: :unauthorized
-    end
-
-
-  private
-  
-    def token_user
-      @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-      @user || errors.add(:token, 'Invalid token') && nil
-    end
-    
-    def decoded_auth_token
-      @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
-    end
-    
-    def http_auth_header
-      if headers['Authorization'].present?
-        return @http_token ||= headers['Authorization'].split(' ').last
-      else
-        errors.add(:token, 'Missing token')
-      end
-      nil
-    end
-
 end
