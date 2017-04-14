72387ec4435862f140bc923494dc36b09e20384a
Kevin Finley <kftwotwo@gmail.com>
2017-04-14 13:53:12 -0700

    fix the products controller with promotions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_product, except: [:index, :create]
   before_action :authorize_product, except: [:index, :create]
-  before_action :build_promotions, only: [:new, :edit]
+  before_action :set_form_resources, only: [:new, :edit]
 
   def index
     @products = policy_scope(Product)
@@ -16,7 +16,7 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    
+
     authorize_product
 
     respond_to do |format|
@@ -24,6 +24,7 @@ class ProductsController < ApplicationController
         format.html { redirect_to @product, success: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
+        set_form_resources
         format.html { render action: :new }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
@@ -39,6 +40,7 @@ class ProductsController < ApplicationController
         format.html { redirect_to @product, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @promotion }
       else
+        set_form_resources
         format.html { render action: :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
       end
@@ -47,24 +49,24 @@ class ProductsController < ApplicationController
 
   def destroy
     @product.destroy
-    
+
     respond_to do |format|
       format.html { redirect_to products_url, notice: "Product successfully destroyed" }
       format.json { head :no_content }
     end
   end
-  
+
   private
-  
+
     def product_params
-      params.require(:product).permit(:featured, 
-                                      :cost, 
-                                      :title, 
-                                      :description, 
-                                      :image, 
+      params.require(:product).permit(:featured,
+                                      :cost,
+                                      :title,
+                                      :description,
+                                      :image,
                                       promotions_attributes: [:id, :_destroy])
     end
-  
+
     def set_product
       @product = params[:id] ? Product.find(params[:id]) : Product.new
     end
@@ -73,9 +75,7 @@ class ProductsController < ApplicationController
       authorize(@product)
     end
 
-    def build_promotions
-      @promotions = policy_scope(Promotion)
-      2.times { @product.promotions.build }
+    def set_form_resources
+      @promotions = Promotion.all
     end
-
 end
