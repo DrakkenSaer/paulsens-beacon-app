faad15daf833f082f7606d4e176f0020ac5f6a74
Kevin Finley <kftwotwo@gmail.com>
2017-04-17 15:18:04 -0700

    fix beacon factory

--- spec/factories/beacon.rb
+++ spec/factories/beacon.rb
@@ -1,6 +1,9 @@
-FactoryGirl.define do 
-    factory :beacon do
-        title "test"
-        description "test description"
-    end
+FactoryGirl.define do
+  factory :beacon do
+    title "test"
+    description "test description"
+    uuid "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+    major_uuid "54381"
+    minor_uuid "53700"
+  end
 end
